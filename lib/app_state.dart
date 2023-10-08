8131fe844cd1e96ddedcff76d64eca0437d9e3fe
Uros <karaleicuu@gmail.com>
2023-10-08 18:21:23 +0200

    app state

---
+++ lib/app_state.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class AppState extends ChangeNotifier{
+  ThemeData themeData=ThemeData(
+    colorScheme: ColorScheme.fromSeed(
+      seedColor: const Color.fromARGB(255, 253, 94, 108),
+      brightness: Brightness.light,
+      
+      primary: const Color.fromARGB(255, 253, 94, 108),
+      onPrimary: Colors.white,
+      
+      secondary: const Color.fromARGB(255, 254, 201, 72),
+      onSecondary: Colors.black,
+
+      tertiary: const Color.fromARGB(255, 253, 163, 85),
+      onTertiary: Colors.white,
+      
+      background: const Color.fromARGB(255, 253, 245, 215),
+      onBackground: Colors.black
+    ),
+    textTheme: TextTheme(
+      headlineMedium: TextStyle(
+        fontWeight: FontWeight.bold,
+        fontSize: 20,
+        color: Colors.black
+      ),
+
+    )
+  );
+
+  AppState(){
+
+  }
+
+  void signIn(String username,String password){}
+  void logIn(String username,String password){}
+  
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:salon/app_state.dart';
 import 'package:salon/pages/main_page.dart';
 
 void main(){
-  runApp(MainPage());
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_)=>AppState())
+      ],
+      child: MainPage()
+    )
+  );
 }
